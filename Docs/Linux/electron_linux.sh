e4636158f3f5bf8589b7eb9e521ee97e88111236
williamhxu <williamhxu@tencent.com>
2026-08-14 16:31:15 +0800

    arm部分国产不支持WEMEET_XWAYLAND

--- Docs/Linux/electron_linux.sh
+++ Docs/Linux/electron_linux.sh
@@ -35,11 +35,11 @@ if [ "$XDG_SESSION_TYPE" = "wayland" ];then
   if [ -f "/opt/x11-wayland/x11-ext.sh" ];then
     source /opt/x11-wayland/x11-ext.sh
   else
+    export WEMEET_XWAYLAND=1
     source ${HERE}/x11-wayland/x11-ext.sh
     export QT_QPA_PLATFORM=xcb
     export XDG_SESSION_TYPE=x11
     unset WAYLAND_DISPLAY
-    export WEMEET_XWAYLAND=1
   fi
 fi
 
